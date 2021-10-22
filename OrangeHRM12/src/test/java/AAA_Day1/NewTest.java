656503e3c10e89077eaa24ea417a7836f5bf8300
dvp291 <durgavaraprasad95@gmail.com>
2021-10-22 15:28:27 +0530

    first commit

---
+++ OrangeHRM12/src/test/java/AAA_Day1/NewTest.java
@@ -0,0 +1,22 @@
+package AAA_Day1;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+import CommonUtil.*;
+public class NewTest {
+	WebDriver driver;
+  
+	 @Test
+	public void LoginTest()  throws Exception
+	 
+	 {
+	  driver=TestBrowser.OpenChromeBrowser();
+	  
+	  
+	  
+	  
+	  
+	 }
+	  
+  }
+
